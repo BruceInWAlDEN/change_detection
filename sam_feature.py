0307efe7b995df4033eddd56bff8c78337c07b95
Bruce Bai <1115941692@qq.com>
2023-06-10 20:38:35 +0800

    sam_feature写了一些

--- sam_feature.py
+++ sam_feature.py
@@ -15,29 +15,94 @@ out_put: [{
 """
 from model.segment_anything.modeling.image_encoder import ImageEncoderViT
 import torch
-
-
-def make_data():
-    pass
+from dataset import Mydata
+import os
 
 
 def make_sam_feature():
-    weight = torch.load('sam_vit_b_01ec64.pth')
+
+    device = torch.device('cuda', index=0)
+
+    weight = torch.load('DATA/sam_vit_b_01ec64.pth')
     weight = {k.replace('image_encoder.', ''): v for k, v in weight.items() if 'image_encoder.' in k}
-    model = ImageEncoderViT(img_size=512)
+    weight_k = list(weight.keys())
+    model = ImageEncoderViT(img_size=1024)
     model_weight = model.state_dict()
-    model_weight.update(weight)
+
+    for k in model_weight.keys():
+        if k in weight_k:
+            model_weight[k] = weight[k]
+        else:
+            print('lost weight: ', k)
+
     model.load_state_dict(model_weight)
     model.eval()
+    model.to(device)
+
+    test_in = torch.rand(4, 3, 1024, 1024)
+    with torch.no_grad():
+        out = model(test_in.to(device))
+    print(out.shape)
+    raise AssertionError
     """
     input: torch.Size([3, 3, 512, 512])
     output: torch.Size([3, 256, 32, 32])
     """
 
+    # 提取所有训练集特征
+    data = Mydata(data_root_dir='DATA/CD_dataset/', c='train')
+    data.batch_size = 1
+    loader = data.get_loader()
 
+    os.mkdir('DATA/CD_dataset/train/sam_feature')
+
+    for im1, im2, label, name in loader:
+        im1 = im1.to(device)
+        im2 = im2.to(device)
+
+        batch_size = len(name)
+        # --> B*2 3 512 512
+        im = torch.cat([im1, im2], dim=0)
+
+        with torch.no_grad():
+
+            # --> B*2 256 32 32
+            feature = model(im)
+            out = feature.detach().clone().cpu()
+
+        for index in range(len(name)):
+
+            # --> 256*2 32 32
+            torch.save(torch.cat([out[index], out[index+batch_size]], dim=0),
+                       os.path.join('DATA/CD_dataset/train/sam_feature/{}.pth'.format(name[index])))
+
+    # 提取所测试集特征
+    data = Mydata(data_root_dir='DATA/CD_dataset/', c='test')
+    data.batch_size = 4
+    loader = data.get_loader()
+
+    os.mkdir('DATA/CD_dataset/test/sam_feature')
+
+    for im1, im2, label, name in loader:
+        im1 = im1.to(device)
+        im2 = im2.to(device)
+
+        batch_size = len(name)
+        # --> B*2 3 512 512
+        im = torch.cat([im1, im2], dim=0)
+
+        with torch.no_grad():
+
+            # --> B*2 256 32 32
+            feature = model(im)
+            out = feature.detach().clone().cpu()
+
+        for index in range(len(name)):
+
+            # --> 256*2 32 32
+            torch.save(torch.cat([feature[index], feature[index+batch_size]], dim=0),
+                       os.path.join('DATA/CD_dataset/test/sam_feature/{}.pth'.format(name[index])))
     pass
-
-
 
 
 if __name__ == '__main__':
